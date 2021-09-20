81de9922fb526fe16bdb7c6b036181c6fe6e3529
kingaguirre <emmanuel@firebox.co.uk>
2021-09-21 00:42:12 +0800

    update

--- src/components/data.tsx
+++ src/components/data.tsx
@@ -1,109 +1,88 @@
 export default [
 {
   ronin: 'ronin:34c97bb8d4a8350a03cdc44eb8eb90f74a66fd80', // king
-  pw: 'asakapa123!@#',
   admin: true,
   share: -1
 },
 {
   ronin: 'ronin:eb11efa52e565e3e0e4139264a0f53824e048275', // jane
-  pw: 'asakapa123!@#',
   admin: true,
   share: 60
 },
 {
   ronin: 'ronin:3b8cf7d0056a6b0b85a4e4ae58f539b706c87159', // sanvel
-  pw: 'Nv=7_nFE@m2gr[yY',
   share: 50
 },
 {
   ronin: 'ronin:0d75bc4d37e01c34bdcc5217d738cd4a10f1a6b0', // mags
-  pw: 'CThW"5mt,Q+8!j3<',
   share: 60
 },
 {
   ronin: 'ronin:f9d3d5bc09cd8860aeb8216254c25cb803809abe', // jhayvel
-  pw: `dD=AXQm^}s2@=V\b`,
   share: 60
 },
 {
   ronin: 'ronin:3ab5aa79be066ec52978fa102ea6e38b650ed3b1', // joy
-  pw: 'Dr;H`]n8VX_}N#{T',
   share: 50
 },
 {
   ronin: 'ronin:5798af891a54591db67cae9181002bc2686d5761', // alex
-  pw: `5mD\C#YBK/..*vVe`,
   share: -1
 },
 {
   ronin: 'ronin:34df8e3f41a51939a063e365a602a64fc8f6586f', // eric
-  pw: '-Y=9nhG9TfVX<bZf',
   share: 60
 },
 {
   ronin: 'ronin:7e721d00685225ae5511a3d3600b92c87c3deaf7', // mark
-  pw: `b2B8j9a'Q}5~a%m{`,
   share: 50
 },
 {
   ronin: 'ronin:a180ab5097a4b4532f5d0099960b14296ff94dc8', // unmada
-  pw: `xW.,<6/F>9GGha>$`,
   share: 100
 },
 {
   ronin: 'ronin:71e339feb065c7e17929dd6db858a7b3e6f18f5b', // tonyo
-  pw: '-4AP*}Qn;v?^=RqN',
   share: 60
 },
 {
   ronin: 'ronin:02ab90f446330d47b78e653b49da8b4886af600a', // cesar
-  pw: '*[]s9?h<N;7NhU-Q',
   share: 60
 },
 {
   ronin: 'ronin:29705d9b6cbf5e6082a40f1574cbcd51974f63ba', // edison
-  pw: '~Ga32r>r(h`xWs.&',
   share: 60
 },
 {
   ronin: 'ronin:5af8386b7e7b72932e173326b664774a42ce4de2', // yram
-  pw: ':cX!S<4]5}g;_3}F',
   share: 60
 },
 {
   ronin: 'ronin:68c314ab9eb774041e9d43d4a5959049009893d0', // abot
-  pw: '7_Ck(RmtgB`{cA^7',
   share: 60
 },
 {
   ronin: 'ronin:5202e03389e7c76660d79ea44574fa49257bf1e4', // paula
-  pw: 'h7GWr&q{cPb$D<gJ',
   share: 60
 },
 {
   ronin: 'ronin:db3136bb44b80ed3d3363ddc623753e41993d27e', // budoy
-  pw: 'L69Ljm68.<r}bu\k',
   share: 50
 },
 {
   ronin: 'ronin:50ee59c52cdfd23cf519ae6f3e294a528bac3409', // miguel
-  pw: 'aLdng9Q8{fjweV&4',
   share: 50
 },
 {
   ronin: 'ronin:5d95c76cac306df5ba63e2838e24800d0175e38c', // james
-  pw: 'Vt8"6Wq]*u;\,=Be',
   share: 60
 },
 {
   ronin: 'ronin:d50116892e9447516ccb42f9897af3c0fe65b6a7', // ryan
-  pw: 'Zn&,R@qH5q[4PKsS',
   share: 50
 },
 {
   ronin: 'ronin:70f1d684258f2f3c0bdc7baa50577f9859acf79b', // frank
-  pw: 'pass: /qp)"c4N%jP+3Aj#',
   share: 50
 },
 ];
