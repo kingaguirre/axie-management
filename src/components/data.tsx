7c10bac12eb6f8c72f580b85207e5c6bfcfc296d
kingaguirre <emmanuel@firebox.co.uk>
2021-11-02 22:59:58 +0800

    update

--- src/components/data.tsx
+++ src/components/data.tsx
@@ -6,10 +6,10 @@ export default [
   {
     ronin: 'ronin:eb11efa52e565e3e0e4139264a0f53824e048275', // jane
     share: 100,
+    otherSlp: 4735,
   },
   {
     ronin: 'ronin:3b8cf7d0056a6b0b85a4e4ae58f539b706c87159', // sanvel
-    otherSlp: 2273
   },
   {
     ronin: 'ronin:0d75bc4d37e01c34bdcc5217d738cd4a10f1a6b0', // mags
@@ -19,7 +19,6 @@ export default [
   },
   {
     ronin: 'ronin:3ab5aa79be066ec52978fa102ea6e38b650ed3b1', // joy
-    otherSlp: 2518
   },
   {
     ronin: 'ronin:5798af891a54591db67cae9181002bc2686d5761', // alex
@@ -30,26 +29,25 @@ export default [
   },
   {
     ronin: 'ronin:7e721d00685225ae5511a3d3600b92c87c3deaf7', // mark
-    otherSlp: 2646
+    otherSlp: 4170
   },
   {
-    ronin: 'ronin:a180ab5097a4b4532f5d0099960b14296ff94dc8', // unmada
+    ronin: 'ronin:a180ab5097a4b4532f5d0099960b14296ff94dc8', // jiangchen
     share: 100,
-    otherSlp: 9634
+    otherSlp: 15652
   },
   {
     ronin: 'ronin:71e339feb065c7e17929dd6db858a7b3e6f18f5b', // tonyo
-    otherSlp: 4168
   },
   {
     ronin: 'ronin:02ab90f446330d47b78e653b49da8b4886af600a', // cesar
-    otherSlp: 4029
   },
   {
     ronin: 'ronin:29705d9b6cbf5e6082a40f1574cbcd51974f63ba', // edison
   },
   {
     ronin: 'ronin:5af8386b7e7b72932e173326b664774a42ce4de2', // justine
+    otherSlp: 1767,
   },
   {
     ronin: 'ronin:68c314ab9eb774041e9d43d4a5959049009893d0', // abot
@@ -59,21 +57,19 @@ export default [
   },
   {
     ronin: 'ronin:db3136bb44b80ed3d3363ddc623753e41993d27e', // budoy
-    otherSlp: 2576
   },
   {
     ronin: 'ronin:50ee59c52cdfd23cf519ae6f3e294a528bac3409', // miguel
-    otherSlp: 1596
   },
   {
     ronin: 'ronin:5d95c76cac306df5ba63e2838e24800d0175e38c', // james
   },
   {
     ronin: 'ronin:d50116892e9447516ccb42f9897af3c0fe65b6a7', // ryan
-    otherSlp: 587
   },
   {
-    ronin: 'ronin:70f1d684258f2f3c0bdc7baa50577f9859acf79b', // frank
+    ronin: 'ronin:70f1d684258f2f3c0bdc7baa50577f9859acf79b', // jc
+    otherSlp: 434,
   },
   {
     ronin: 'ronin:5ed1cd1f60f7b4893ff07b8ca7304efeeab1408f', // kyrie
@@ -86,6 +82,7 @@ export default [
   },
   {
     ronin: 'ronin:6b88145556c6411900d0fffbf48afff522fec092', // jhie
+    otherSlp: 1555,
   },
   {
     ronin: 'ronin:552af96a9089402accfedda84952338fde3b6921', // rivera
